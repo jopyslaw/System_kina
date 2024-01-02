28b0407210a63c6f16db91c7e0616554b615bc39
jopyslaw <jopyslaw@gmail.com>
2024-01-02 17:40:21 +0100

    Change BPMN schema

--- src/main/java/com/CinemaProject/cinemaProject/reservation/ReservationController.java
+++ src/main/java/com/CinemaProject/cinemaProject/reservation/ReservationController.java
@@ -43,9 +43,6 @@ public class ReservationController {
                 .latestVersion()
                 .variables(variables)
                 .send();
-
-
-
     }
 
     @GetMapping("/confirmPayment")
